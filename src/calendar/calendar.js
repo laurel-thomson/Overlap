edebd8d3df416839b8ae76dd0b707a800cb9a2db
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-02 14:44:59 -0700

    Fixing error where the wrong dimensions of cells array was being passed to the calendar component

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,29 +1,14 @@
-import React from 'react';
-import TableDragSelect from 'react-table-drag-select';
-import 'react-table-drag-select/style.css';
-import './calendar.css'
-import TimeSlot from './timeslot.js'
+import React from 'react'
+import TableDragSelect from "react-table-drag-select";
+import "react-table-drag-select/style.css";
 
 export default class Calendar extends React.Component {
   constructor(props) {
     super(props);
 
-    const days = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday'
-    ];
-    const startTime = 8;
-    const endTime = 5;
-    const numRows = 8;
-
     let cells = [];
-    for (let i = 0; i < numRows + 1; i++) {
-      cells.push(Array(days.length+1).fill(false));
+    for (let i = 0; i < 8; i++) {
+      cells.push(Array(7).fill(false));
     }
 
     this.state = {
@@ -32,79 +17,82 @@ export default class Calendar extends React.Component {
   }
 
   render = () =>
-    <TableDragSelect
-      value={this.state.cells}
-      onChange={cells => this.setState({ cells })}
-    >
-      <tr>
-       <td disabled />
-       <td disabled>Monday</td>
-       <td disabled>Tuesday</td>
-       <td disabled>Wednesday</td>
-       <td disabled>Thursday</td>
-       <td disabled>Friday</td>
-       <td disabled>Saturday</td>
-       <td disabled>Sunday</td>
-     </tr>
-      <tr>
-        <td disabled>10:00</td>
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-      </tr>
-      <tr>
-        <td disabled>11:00</td>
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-      </tr>
-      <tr>
-        <td disabled>12:00</td>
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-      </tr>
-      <tr>
-        <td disabled>13:00</td>
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-      </tr>
-      <tr>
-        <td disabled>14:00</td>
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-      </tr>
-      <tr>
-        <td disabled>15:00</td>
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-        <TimeSlot />
-      </tr>
-    </TableDragSelect>;
+    <div>
+      <TableDragSelect value={this.state.cells} onChange={this.handleChange}>
+        <tr>
+          <td disabled />
+          <td disabled>Monday</td>
+          <td disabled>Tuesday</td>
+          <td disabled>Wednesday</td>
+          <td disabled>Thursday</td>
+          <td disabled>Friday</td>
+          <td disabled>Saturday</td>
+        </tr>
+        <tr>
+          <td disabled>10:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>11:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>12:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>13:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>14:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>15:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+        <tr>
+          <td disabled>16:00</td>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
+          <td>overtime</td>
+        </tr>
+      </TableDragSelect>
+    </div>;
+
+  handleChange = cells => this.setState({ cells });
 }
--- src/calendar/timeslot.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TimeSlot(props){
-    return (
-      <td/>
-    )
-}
