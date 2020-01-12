f17c3273450821daefae6cbdf2135f64432db4d0
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-12 16:21:13 -0700

    Passing schedule name to create schedule component

--- client/src/home/CreateScheduleLink.js
+++ client/src/home/CreateScheduleLink.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 export default class CreateScheduleLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {scheduleName: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({scheduleName : event.target.value});
+  }
+
   render() {
     return (
-      <Link to={{pathname: '/create', scheduleName: 'hello'}}>Create Schedule!</Link>
+        <div>
+         <label>
+           Schedule Name:
+           <input type="text" scheduleName={this.state.scheduleName} onChange={this.handleChange} />
+         </label>
+
+         <Link to={{pathname: '/create', scheduleName: `${this.state.scheduleName}` }}>Create Schedule!</Link>
+        </div>
     );
   }
 }
