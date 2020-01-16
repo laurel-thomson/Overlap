8d29eec26b681b76e65152f852599d6406dc9b41
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-16 07:11:31 -0700

    Styling the home page

--- client/src/home/HomeLink.js
+++ client/src/home/HomeLink.js
@@ -21,7 +21,7 @@ export default class CreateScheduleLink extends React.Component {
         <h4>{this.props.task} a schedule</h4>
         <div className='form'>
           <div className='icon'>
-           <FontAwesomeIcon icon={this.props.icon} color='white'/>
+           <FontAwesomeIcon icon={this.props.icon} className='circleIcon' color='white'/>
           </div>
           <input type='text' value={this.state.inputValue} placeholder={this.props.placeholder} onChange={this.handleChange} />
           <Link to={{pathname: '/create', inputValue: `${this.state.inputValue}` }}><FontAwesomeIcon icon='angle-right' className='arrow'/></Link>
