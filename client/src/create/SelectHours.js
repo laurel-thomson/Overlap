18f6db91fc5f9589b8decc3e44643ead43dedb2f
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-18 11:49:22 -0700

    Working on SelectHours component

--- client/src/create/SelectHours.js
+++ client/src/create/SelectHours.js
@@ -4,7 +4,18 @@ export default class SelectHours extends React.Component {
   render() {
     return (
       <div>
-        <p>Select Hours</p>
+        <p>select possible hours for</p>
+        <p>{this.props.eventName}</p>
+        <label for="startTime">Start Time</label>
+        <input name="startTime" type="time" />
+        <label for="endTime">End Time</label>
+        <input name="endTime" type="time" />
+        <label for="interval">Time Slot Length</label>
+        <select>
+          <option name="15">15 minutes</option>
+          <option name="30">30 minutes</option>
+          <option name="60">60 minutes</option>
+        </select>
         <button onClick={() => this.props.handleClick('newaccesscode')}>DONE</button>
       </div>
     );
