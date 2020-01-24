788d55c9b3a9bccc4e72e09eb763b42b75c6c5ae
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-24 07:12:12 -0700

    Adding copy to clipboard functionality

--- client/src/calendar/CodeCopy.js
+++ client/src/calendar/CodeCopy.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './CodeCopy.css';
 
-export default function CodeCopy(props) {
-  return (
-    <div className='code-copy'>
-      <div className='label'>{props.label}</div>
-      <button
-        className='clipboard-code'
-        aria-label='copy to clipboard'
-      >
-        {props.code}
-        <FontAwesomeIcon icon='clipboard' className='clipboard'/>
-      </button>
-    </div>
-  );
+export default class CodeCopy extends React.Component {
+
+  copyCode = (event) => {
+    var textField = document.createElement('textarea');
+    textField.innerText = this.props.code;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  };
+
+  render() {
+    return (
+      <div className='code-copy'>
+        <div className='label'>{this.props.label}</div>
+        <button
+          className='clipboard-code'
+          aria-label='copy to clipboard'
+          onClick={this.copyCode}
+        >
+          {this.props.code}
+          <FontAwesomeIcon icon='clipboard' className='clipboard'/>
+        </button>
+      </div>
+    );
+  }
 }
