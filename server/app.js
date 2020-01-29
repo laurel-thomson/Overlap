adfac75322354c23277dde1ea1fe791838769f08
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-28 22:57:05 -0700

    Sending GET call to API and only rendering the calendar if the access code is found

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const firebase = require('firebase');
 const config = {
