c480136313645e1b7ec495769b52155d456585f1
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-01-30 06:26:09 -0700

    Dynamically calculating overlap

--- client/src/calendar/schedule/OverlapDay.js
+++ client/src/calendar/schedule/OverlapDay.js
@@ -4,17 +4,6 @@ import OverlapSlot from './OverlapSlot.js';
 export default class OverlapDay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      slots : [
-         { name : '08:30', overlap : 0 },
-         { name : '09:00', overlap : 5 },
-         { name : '09:30', overlap : 7 },
-         { name : '10:00', overlap : 10 },
-         { name : '10:30', overlap : 3 },
-         { name : '11:00', overlap : 4 },
-      ],
-      totalUsers: 10
-    }
   }
 
   render() {
@@ -23,12 +12,12 @@ export default class OverlapDay extends React.Component {
         <h3>{this.props.name}</h3>
         <hr className='header-line' />
         <div className='time-slots'>
-          {this.state.slots.map((slot, index) =>
+          {this.props.timeslots.map((slot, index) =>
             <OverlapSlot
               key={index}
               name={slot.name}
               overlap={slot.overlap}
-              totalUsers={this.state.totalUsers}
+              totalUsers={this.props.totalUsers}
             />
           )}
         </div>
--- client/src/calendar/schedule/OverlapSchedule.js
+++ client/src/calendar/schedule/OverlapSchedule.js
@@ -3,12 +3,20 @@ import OverlapDay from './OverlapDay.js'
 import './Schedule.css';
 
 export default class OverlapSchedule extends React.Component {
+  calculateSchedule = () => {
+    const totalUsers = this.props.users;
+    const schedule = this.props.schedule.days;
+    return Object.keys(schedule).map((day) => {
+      const timeslots = schedule[day].map((index) => {
+        return { name : index.time, overlap : index.users ? Object.keys(index.users).length : 0 };
+      });
+      return <OverlapDay key={day} name={day} timeslots={timeslots} totalUsers={totalUsers.length} />
+    });
+  }
   render() {
     return (
       <div className='schedule overlap'>
-        <OverlapDay name='Sunday' />
-        <OverlapDay name='Monday' />
-        <OverlapDay name='Tuesday' />
+        {this.calculateSchedule()}
       </div>
     );
   }
