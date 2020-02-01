8109b5a9a8418791b799c44af78af3e8f2fbad61
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-02-01 08:34:58 -0700

    Makign the overlap opacity light if there are no users

--- client/src/calendar/schedule/OverlapSlot.js
+++ client/src/calendar/schedule/OverlapSlot.js
@@ -4,6 +4,9 @@ import './Schedule.css';
 export default class OverlapSlot extends React.Component {
   render() {
     let opacity = (this.props.overlap + 1) / (this.props.totalUsers + 1);
+    if (this.props.totalUsers === 0) {
+      opacity = 0.25;
+    }
     return (
       <div className='timeslot overlap'>
         <p>{this.props.name}</p>
