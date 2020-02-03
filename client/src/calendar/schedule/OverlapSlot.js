d34529a92945b1c7f4e7ad5859bb082a91ef7e68
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-02-02 20:04:15 -0700

    Making tooltip mobile friendly

--- client/src/calendar/schedule/OverlapSlot.js
+++ client/src/calendar/schedule/OverlapSlot.js
@@ -3,14 +3,34 @@ import './Schedule.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class OverlapSlot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tooltipOpen : false
+    }
+  }
+
+  toggleTooltip = () => {
+    if (window.innerWidth >= 1024) return;
+    this.setState({
+      tooltipOpen : !this.state.tooltipOpen
+    });
+  }
+
+  closeTooltip = () => {
+    this.setState({
+      tooltipOpen : false
+    });
+  }
+
   getTooltip = () => {
     const users = Object.keys(this.props.users);
     if (users.length > 0) {
       return (
-        <div className='tooltip'>
+        <div className={`tooltip ${this.state.tooltipOpen ? 'open' : ''}`}>
           {Object.keys(this.props.users).map((user) => {
             return (
-              <div className='tooltip-user'>
+              <div className={'tooltip-user'} key={user}>
                 <div className='icon'>
                  <FontAwesomeIcon icon='user' className='circleIcon' color='white' aria-hidden='true'/>
                 </div>
@@ -18,6 +38,9 @@ export default class OverlapSlot extends React.Component {
               </div>
             );
           })}
+          <button aria-label='close' className='close-button' onClick={this.closeTooltip}>
+            <FontAwesomeIcon icon='times' color='white' aria-hidden='true' />
+          </button>
         </div>
       );
     }
@@ -32,7 +55,7 @@ export default class OverlapSlot extends React.Component {
     return (
       <div className={`timeslot overlap ${overlap > 0 ? 'has-tooltip' : ''}`}>
         <p>{this.props.name}</p>
-        <button style={{'backgroundColor' : `rgba(15, 158, 196, ${opacity})`}}>
+        <button style={{'backgroundColor' : `rgba(15, 158, 196, ${opacity})`}} onClick={this.toggleTooltip}>
           {overlap} / {this.props.totalUsers}
         </button>
         {this.getTooltip()}
